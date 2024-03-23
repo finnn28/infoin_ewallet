23afa7cbab470916033c322449069fa7dfa1c936
apanyaclay <apanyaclay@gmail.com>
2024-03-24 01:29:41 +0700

    refactor: Update code login.dart Mengupdate halaman login dengan menambahkan form dan sebagainya

--- lib/Auth/login.dart
+++ lib/Auth/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:infoin_ewallet/Auth/register.dart';
+import 'package:infoin_ewallet/Pages/home.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -10,6 +12,61 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const  EdgeInsets.symmetric(horizontal: 40),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding:const  EdgeInsets.symmetric(horizontal: 40),
+              child: TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add login functionality here
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> const Home()));
+              },
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                // Navigate to Register screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const  Register()),
+                );
+              },
+              child: const  Text('Create an account'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
